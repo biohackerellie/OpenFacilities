f95bad03cd6ab6781dda37d0955e2e6d942e0388
biohackerellie <epkerns@gmail.com>
2023-09-16 21:54:19 -0600

    i hate css

--- front-end/src/components/ui/tables/index.ts
+++ front-end/src/components/ui/tables/index.ts
@@ -1,6 +1,5 @@
-import AdminResSummary from './AdminResSummary';
 import ResSummary from './ResSummary';
 import ReservationTable from './reservationTable';
 import { DataTable } from './reservations/data-table';
 
-export { AdminResSummary, ResSummary, ReservationTable, DataTable };
+export { ResSummary, ReservationTable, DataTable };
