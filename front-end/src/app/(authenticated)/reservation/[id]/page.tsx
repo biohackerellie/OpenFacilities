a4cd4d653d08b09bc8b2f5e4113543484e44eb03
biohackerellie <epkerns@gmail.com>
2023-10-02 11:48:42 -0600

    fix

--- front-end/src/app/(authenticated)/reservation/[id]/page.tsx
+++ front-end/src/app/(authenticated)/reservation/[id]/page.tsx
@@ -36,74 +36,84 @@ export default async function reservationPage({
 
   const startDate = ReservationDate[0].startDate;
   const facility = Facility.id;
-	let mappedDates;
+  let mappedDates;
 
   if (ReservationDate.length === 0) {
     mappedDates = ReservationDate.map((date: DateType) => {
-    return {
-      Options: date.id,
-      startDate: date.startDate,
-      endDate: date.endDate,
-      startTime: date.startTime,
-      endTime: date.endTime,
-      approved: date.approved,
-      ReservationID: date.reservationId,
-    };
-  });
+      return {
+        Options: date.id,
+        startDate: date.startDate,
+        endDate: date.endDate,
+        startTime: date.startTime,
+        endTime: date.endTime,
+        approved: date.approved,
+        ReservationID: date.reservationId,
+      };
+    });
 
-  return (
-    <div className="flex flex-wrap justify-center h-full pb-3 mb-2 ">
-      <div key={facility} className="     ">
-        <div className="  xl:w-[1300px] w-[800px] gap-y-4  drop-shadow-md  m-3 p-4 ">
-          <div>
-            <h2 className="  font-bold gap-y-4 text-xl text-gray-600 dark:text-gray-300">
-              {' '}
-              Summary{' '}
-            </h2>
-          </div>
-          <div className="justify-between  my-5  gap-36">
-            <div className="flex flex-row  justify-between text-lg border-b-2   text-justify ">
-              Primary Contact: {primaryContact} <div> {name}</div>
-            </div>
-            <div className="flex flex-row  justify-between text-lg border-b-2   text-justify ">
-              Contact Number: <div>{phone}</div>
-            </div>
-            <div className="flex flex-row  justify-between text-lg border-b-2   text-justify ">
-              Contact Email: <div>{reservation.email}</div>
-            </div>
-            <div className="flex flex-row  justify-between text-lg border-b-2   text-justify ">
-              Requested Category:{' '}
-              <div className="truncate overflow-ellipsis max-w-sm">
-                {Category.name}
-              </div>
-            </div>
-            <div className="flex flex-row my-10 text-ellipsis flex-wrap gap-10 justify-between text-xl border-b-2  text-justify">
-              Description:{' '}
-              <div className="text-left ml-10 flex text-md text-ellipsis">
-                {details}{' '}
-              </div>
-            </div>
-            <div className="container max-w-[600px] float-left ">
-              <h1 className="font-bold text-xl p-4 m-3 text-gray-600 dark:text-gray-300">
+    return (
+      <div className="flex flex-wrap justify-center h-full pb-3 mb-2 ">
+        <div key={facility} className="     ">
+          <div className="  xl:w-[1300px] w-[800px] gap-y-4  drop-shadow-md  m-3 p-4 ">
+            <div>
+              <h2 className="  font-bold gap-y-4 text-xl text-gray-600 dark:text-gray-300">
                 {' '}
-                {Facility.name} calendar{' '}
-              </h1>
-							{!startDate && <div>User did not submit reservation dates on request. Please resubmit</div>}
-							{startDate &&
-
-              <SmallCalendar startDate={startDate} facilityId={Facility.id} />
-							}
+                Summary{' '}
+              </h2>
             </div>
-          </div>
-          <div className="max-w-[650px] float-right ">
-            <h2 className="font-bold text-xl p-4 m-3 text-gray-600 dark:text-gray-300">
-              Reservation Dates
-            </h2>
-						{!mappedDates && <div>Loading...</div>}
-          {mappedDates && <DataTable columns={columns} data={mappedDates} />}
+            <div className="justify-between  my-5  gap-36">
+              <div className="flex flex-row  justify-between text-lg border-b-2   text-justify ">
+                Primary Contact: {primaryContact} <div> {name}</div>
+              </div>
+              <div className="flex flex-row  justify-between text-lg border-b-2   text-justify ">
+                Contact Number: <div>{phone}</div>
+              </div>
+              <div className="flex flex-row  justify-between text-lg border-b-2   text-justify ">
+                Contact Email: <div>{reservation.email}</div>
+              </div>
+              <div className="flex flex-row  justify-between text-lg border-b-2   text-justify ">
+                Requested Category:{' '}
+                <div className="truncate overflow-ellipsis max-w-sm">
+                  {Category.name}
+                </div>
+              </div>
+              <div className="flex flex-row my-10 text-ellipsis flex-wrap gap-10 justify-between text-xl border-b-2  text-justify">
+                Description:{' '}
+                <div className="text-left ml-10 flex text-md text-ellipsis">
+                  {details}{' '}
+                </div>
+              </div>
+              <div className="container max-w-[600px] float-left ">
+                <h1 className="font-bold text-xl p-4 m-3 text-gray-600 dark:text-gray-300">
+                  {' '}
+                  {Facility.name} calendar{' '}
+                </h1>
+                {!startDate && (
+                  <div>
+                    User did not submit reservation dates on request. Please
+                    resubmit
+                  </div>
+                )}
+                {startDate && (
+                  <SmallCalendar
+                    startDate={startDate}
+                    facilityId={Facility.id}
+                  />
+                )}
+              </div>
+            </div>
+            <div className="max-w-[650px] float-right ">
+              <h2 className="font-bold text-xl p-4 m-3 text-gray-600 dark:text-gray-300">
+                Reservation Dates
+              </h2>
+              {!mappedDates && <div>Loading...</div>}
+              {mappedDates && (
+                <DataTable columns={columns} data={mappedDates} />
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
