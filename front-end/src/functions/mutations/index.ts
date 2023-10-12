3e14fea196487d9705e05082f9800dfb79817cd7
biohackerellie <epkerns@gmail.com>
2023-10-12 11:01:43 -0600

    last time

--- front-end/src/functions/mutations/index.ts
+++ front-end/src/functions/mutations/index.ts
@@ -3,18 +3,9 @@ import Paid from './paid';
 
 import Uploader from './uploader';
 import addFee from './addFee';
-import verifyDoc from './verifyDoc';
+
 import { changeCat } from './change-category';
 import removeFee from './remove-fee';
 import modifyDate from './modifyDate';
 
-export {
-  modifyDate,
-  CreateUser,
-  Paid,
-  Uploader,
-  verifyDoc,
-  addFee,
-  changeCat,
-  removeFee,
-};
+export { modifyDate, CreateUser, Paid, Uploader, addFee, changeCat, removeFee };
