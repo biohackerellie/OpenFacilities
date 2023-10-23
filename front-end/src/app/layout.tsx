52bce3033b0d2a2fd7f6f09a92658799c0adb074
biohackerellie <epkerns@gmail.com>
2023-10-23 09:02:16 -0600

    Add Google Analytics to RootLayout component

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Providers } from '@/components/contexts';
 import Footer from '@/components/ui/footer';
 import Navbar from '@/components/ui/navbar/Navbar';
 import moment from 'moment-timezone';
+import GoogleAnalytics from '@/lib/GoogleAnalytics';
 import { Analytics } from '@vercel/analytics/react';
 import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
@@ -12,37 +13,38 @@ import './globals.css';
 moment.tz.setDefault('America/Denver');
 
 const roboto_mono = Roboto_Mono({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-roboto-mono',
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-roboto-mono',
 });
 
 export const metadata = {
-  title: 'LPS Facilities',
-  description: 'Laurel Public Schools Facility Rentals',
+	title: 'LPS Facilities',
+	description: 'Laurel Public Schools Facility Rentals',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <AuthProvider>
-      <html lang="en" suppressHydrationWarning>
-        <body className={`${roboto_mono.variable}`}>
-          <ThemeProviders>
-            <Providers>
-              <Navbar />
-              {children}
+	return (
+		<AuthProvider>
+			<html lang="en" suppressHydrationWarning>
+				<GoogleAnalytics GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_TRACKING_ID as string} />
+				<body className={`${roboto_mono.variable}`}>
+					<ThemeProviders>
+						<Providers>
+							<Navbar />
+							{children}
 
-              <Footer />
-              <Toaster />
-            </Providers>
-          </ThemeProviders>
-          <Analytics />
-        </body>
-      </html>
-    </AuthProvider>
-  );
+							<Footer />
+							<Toaster />
+						</Providers>
+					</ThemeProviders>
+					<Analytics />
+				</body>
+			</html>
+		</AuthProvider>
+	);
 }
