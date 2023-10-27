b868fe08300963b6bb3f4b00a6d7e2c17f4e2990
biohackerellie <epkerns@gmail.com>
2023-10-27 08:30:11 -0600

    Add webpack configuration to ignore fs and path modules.

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -6,6 +6,16 @@ dns.setDefaultResultOrder('ipv4first');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+	webpack(config, { nextRuntime }) {
+		if ( typeof nextRuntime === 'undefined' ) {
+			const { IgnorePlugin } = require('webpack');
+			const ignoreFs = new IgnorePlugin({ resourceRegExp: /fs/ });
+			const ignorePath = new IgnorePlugin({ resourceRegExp: /path/ });
+			config.plugins.push(ignoreFs, ignorePath);
+		}
+		return config;
+		
+	}
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
