3c5e6f5812cb1514cd334741b3fe136bba48efb8
biohackerellie <epkerns@gmail.com>
2023-11-03 11:46:06 -0600

    modified:   front-end/drizzle/schema.ts

--- front-end/drizzle/schema.ts
+++ front-end/drizzle/schema.ts
@@ -1,24 +1,10 @@
-import { pgTable, pgEnum, varchar, timestamp, text, integer } from "drizzle-orm/pg-core"
+import {
+  pgTable,
+  pgEnum,
+  varchar,
+  timestamp,
+  text,
+  integer,
+} from 'drizzle-orm/pg-core';
 
-import { sql } from "drizzle-orm"
-export const keyStatus = pgEnum("key_status", ['default', 'valid', 'invalid', 'expired'])
-export const keyType = pgEnum("key_type", ['aead-ietf', 'aead-det', 'hmacsha512', 'hmacsha256', 'auth', 'shorthash', 'generichash', 'kdf', 'secretbox', 'secretstream', 'stream_xchacha20'])
-export const factorType = pgEnum("factor_type", ['totp', 'webauthn'])
-export const factorStatus = pgEnum("factor_status", ['unverified', 'verified'])
-export const aalLevel = pgEnum("aal_level", ['aal1', 'aal2', 'aal3'])
-export const codeChallengeMethod = pgEnum("code_challenge_method", ['s256', 'plain'])
-export const reservationApproved = pgEnum("Reservation_approved", ['pending', 'approved', 'denied', 'canceled'])
-export const reservationDateApproved = pgEnum("ReservationDate_approved", ['pending', 'approved', 'denied', 'canceled'])
-export const userRole = pgEnum("User_role", ['CAL_ADMIN', 'ADMIN_ADMIN', 'GR_ADMIN', 'LHS_ADMIN', 'LMS_ADMIN', 'WE_ADMIN', 'SO_ADMIN', 'SUP_ADMIN', 'USER'])
-
-
-export const prismaMigrations = pgTable("_prisma_migrations", {
-	id: varchar("id", { length: 36 }).primaryKey().notNull(),
-	checksum: varchar("checksum", { length: 64 }).notNull(),
-	finishedAt: timestamp("finished_at", { withTimezone: true, mode: 'string' }),
-	migrationName: varchar("migration_name", { length: 255 }).notNull(),
-	logs: text("logs"),
-	rolledBackAt: timestamp("rolled_back_at", { withTimezone: true, mode: 'string' }),
-	startedAt: timestamp("started_at", { withTimezone: true, mode: 'string' }).defaultNow().notNull(),
-	appliedStepsCount: integer("applied_steps_count").default(0).notNull(),
-});
+import { sql } from 'drizzle-orm';
