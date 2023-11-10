0478d8bdab1abb72654bb517c6ed7a4883e0e58f
biohackerellie <epkerns@gmail.com>
2023-11-10 10:59:58 -0700

    Fix reservation insurance page and remove unused runtime declaration

--- front-end/src/app/api/events/route.ts
+++ front-end/src/app/api/events/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server';
 import { EventsQuery } from '@/lib/db/queries/events';
 import { serializeJSON } from '@/utils/serializeJSON';
 
-export const runtime = 'edge';
 export async function GET(request: Request) {
   const res = await EventsQuery.execute();
   return NextResponse.json(serializeJSON(res));
