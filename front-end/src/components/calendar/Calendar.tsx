43a9179bcb5321ebe2ceaeb69d0f6fc59a3c5626
biohackerellie <epkerns@gmail.com>
2023-11-15 14:03:46 -0700

    more work on the calendar

--- front-end/src/components/calendar/Calendar.tsx
+++ front-end/src/components/calendar/Calendar.tsx
@@ -1,4 +1,3 @@
-//@ts-nocheck
 'use client';
 import React, { useEffect, useState, useRef } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
@@ -6,14 +5,19 @@ import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import ReactModal from 'react-modal';
 import { CalendarInfo } from '../ui';
-
+import BuildingFilter from './navigation/filterBar';
+import { Facility } from '@/lib/db/schema';
 import { useTheme } from 'next-themes';
 import { Button } from '../ui/buttons';
-import { Separator } from '@/components/ui/separator';
+import { useSearchParams } from 'next/navigation';
 
+import {
+  buildingCalendars,
+  type BuildingCalendars,
+} from '@/lib/types/calendars';
 const localizer = momentLocalizer(moment);
 
-const buildingColors: any = {
+const buildingColors: Record<string, string> = {
   'West Elementary': 'purple',
   'South Elementary': 'blue',
   'Laurel Middle School': 'green',
@@ -22,45 +26,39 @@ const buildingColors: any = {
   'Administration Building': 'orange',
 };
 
-function EventComponent({ event }: { event: Events }) {
+type BuildingColors = keyof typeof buildingColors;
+type SelectFacility = typeof Facility.$inferSelect;
+type Event = {
+  title: string;
+  start: Date;
+  end: Date;
+  building: any;
+  Facility: SelectFacility;
+};
+
+function EventComponent({ event }: { event: Event }) {
   return (
     <div
-      className={`rbc-event-content ${
-        buildingColors[event.building]
-      } rbc-event-label`}
-      title={event.title}
+      className={`${buildingColors[event?.building]} rbc-event-label`}
+      title={event?.title}
     >
-      {event.title}
+      {event?.title}
     </div>
   );
 }
 
-export default function CalendarMain({ fetchedEvents }) {
-  const buildingCalendars = {
-    Master:
-      'https://calendar.google.com/calendar/embed?src=c_a55b94eb4dd05e5dd936dd548d434d6a25c2694efe67224e3eff10205d2fb82b%40group.calendar.google.com&ctz=America%2FDenver',
-    'West Elementary':
-      'https://calendar.google.com/calendar/embed?src=c_0b2e2e109de17b0bfb36c10b64324c2d4ddcaa19c12998aca1cd5a77ac33e8a5%40group.calendar.google.com&ctz=America%2FDenver',
-    'South Elementary':
-      'https://calendar.google.com/calendar/embed?src=c_177688d95252d6bd257327592bb3802966de1b52b47c2ad4bc94232738ece835%40group.calendar.google.com&ctz=America%2FDenver',
-    'Laurel Middle School':
-      'https://calendar.google.com/calendar/embed?src=c_8eff489230617e6c41b5e58bc6a617df6c9cbce18e87042c2e56a2db148c5719%40group.calendar.google.com&ctz=America%2FDenver',
-    'Laurel High School':
-      'https://calendar.google.com/calendar/embed?src=c_01cc33d2abc19e76a8fc604964d68670ec62556b2e2b761bce275f8ede807792%40group.calendar.google.com&ctz=America%2FDenver',
-    'Graff Elementary':
-      'https://calendar.google.com/calendar/embed?src=c_419e3a96109d8a6a3cd718fa3583f1a105c39f9d9ecff712f2caabfb549ee2d9%40group.calendar.google.com&ctz=America%2FDenver',
-    'Administration Building':
-      'https://calendar.google.com/calendar/embed?src=c_1885ejmm84vl8g0pmttlucapva16e%40resource.calendar.google.com&ctz=America%2FDenver',
-    'Laurel Stadium':
-      'https://calendar.google.com/calendar/embed?src=c_188f41n72e9d0h33l3n96tt6jg9t6%40resource.calendar.google.com&ctz=America%2FDenver',
-  };
+export default function CalendarMain({
+  fetchedEvents,
+}: {
+  fetchedEvents: Event[];
+}) {
+  const searchParams = useSearchParams();
 
-  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [selectedEvent, setSelectedEvent] = useState<Event>(null);
 
-  const [selectedBuilding, setSelectedBuilding] = useState('Master');
-  const handleSetSelectedBuilding = (building: string) => {
-    setSelectedBuilding(building);
-  };
+  const selectedBuilding: BuildingCalendars =
+    (searchParams.get('building') as BuildingCalendars) || 'All';
+
   const { theme } = useTheme();
 
   const isDarkMode = theme === 'dark';
@@ -74,13 +72,14 @@ export default function CalendarMain({ fetchedEvents }) {
     }),
   };
 
-  const mappedEvents = fetchedEvents.map((event) => ({
-    title: event.title,
-    start: new Date(event.start),
-    end: new Date(event.end),
-    building: event.Facility.building,
-    facility: event.Facility.name,
+  const mappedEvents = fetchedEvents.map((event: Event) => ({
+    title: event?.title,
+    start: new Date(event?.start as unknown as string),
+    end: new Date(event?.end as unknown as string),
+    building: event?.Facility.building,
+    facility: event?.Facility.name,
   }));
+  type MappedEvents = keyof typeof mappedEvents;
 
   useEffect(() => {
     if (selectedEvent) {
@@ -91,126 +90,13 @@ export default function CalendarMain({ fetchedEvents }) {
   }, [selectedEvent]);
 
   const filteredEvents =
-    selectedBuilding === 'Master'
+    selectedBuilding === 'All'
       ? mappedEvents
       : mappedEvents.filter((event) => event.building === selectedBuilding);
 
   return (
     <div>
-      <div className="hidden sm:visible sm:flex h-5 justify-center items-center space-x-4 text-sm">
-        <div className="">
-          <Button
-            variant="link"
-            className={`${
-              selectedBuilding === 'Master'
-                ? 'text-blue-500'
-                : 'text-foreground'
-            }`}
-            onClick={() => handleSetSelectedBuilding('Master')}
-          >
-            All Events
-          </Button>
-        </div>
-        <Separator orientation="vertical" />
-        <div>
-          <Button
-            variant="link"
-            className={`${
-              selectedBuilding === 'Graff Elementary'
-                ? 'text-blue-500'
-                : 'text-foreground'
-            }`}
-            onClick={() => handleSetSelectedBuilding('Graff Elementary')}
-          >
-            Graff
-          </Button>
-        </div>
-        <Separator orientation="vertical" />
-        <div>
-          <Button
-            variant="link"
-            className={`${
-              selectedBuilding === 'Laurel High School'
-                ? 'text-blue-500'
-                : 'text-foreground'
-            }`}
-            onClick={() => handleSetSelectedBuilding('Laurel High School')}
-          >
-            {' '}
-            Laurel High School
-          </Button>
-        </div>
-        <Separator orientation="vertical" />
-        <div>
-          <Button
-            variant="link"
-            className={`${
-              selectedBuilding === 'Laurel Middle School'
-                ? 'text-blue-500'
-                : 'text-foreground'
-            }`}
-            onClick={() => handleSetSelectedBuilding('Laurel Middle School')}
-          >
-            Laurel Middle School
-          </Button>
-        </div>
-        <Separator orientation="vertical" />
-        <div>
-          <Button
-            variant="link"
-            className={`${
-              selectedBuilding === 'West Elementary'
-                ? 'text-blue-500'
-                : 'text-foreground'
-            }`}
-            onClick={() => handleSetSelectedBuilding('West Elementary')}
-          >
-            West Elementary
-          </Button>
-        </div>
-        <Separator orientation="vertical" />
-        <div>
-          <Button
-            variant="link"
-            className={`${
-              selectedBuilding === 'South Elementary'
-                ? 'text-blue-500'
-                : 'text-foreground'
-            }`}
-            onClick={() => handleSetSelectedBuilding('South Elementary')}
-          >
-            South Elementary
-          </Button>
-        </div>
-        <Separator orientation="vertical" />
-        <div>
-          <Button
-            variant="link"
-            className={`${
-              selectedBuilding === 'Administration Building'
-                ? 'text-blue-500'
-                : 'text-foreground'
-            }`}
-            onClick={() => handleSetSelectedBuilding('Administration Building')}
-          >
-            Administration Building
-          </Button>
-        </div>
-        <Separator orientation="vertical" />
-        <div>
-          <Button
-            variant="link"
-            className={`${
-              selectedBuilding === 'Laurel Stadium'
-                ? 'text-blue-500'
-                : 'text-foreground'
-            }`}
-            onClick={() => handleSetSelectedBuilding('Laurel Stadium')}
-          >
-            Laurel Stadium
-          </Button>
-        </div>
-      </div>
+      <BuildingFilter />
       <div className=" h-screen block   items-center   mx-auto p-[10px]  align-center  ">
         <div className="drop-shadow-md ">
           <div className=" flex justify-center   ">
@@ -225,6 +111,7 @@ export default function CalendarMain({ fetchedEvents }) {
           </div>
           <Calendar
             localizer={localizer}
+            //@ts-expect-error
             events={filteredEvents}
             onSelectEvent={(event) => setSelectedEvent(event)}
             popup
@@ -235,7 +122,6 @@ export default function CalendarMain({ fetchedEvents }) {
             })}
             startAccessor="start"
             endAccessor="end"
-            // className="z-0 bg-transparent  font-normal m-auto border-solid border-4 backdrop-blur-lg object-center rounded-lg max-h-auto sm:max-h-auto    text-black max-w-[1080px] dark:text-white"
             style={calendarStyle}
             components={{
               event: EventComponent,
@@ -252,6 +138,7 @@ export default function CalendarMain({ fetchedEvents }) {
             <div className="bg-white rounded-lg p-8">
               <h3 className="text-xl font-bold mb-4">{selectedEvent?.title}</h3>
               <h4 className="text-lg mb-2">{selectedEvent?.building}</h4>
+
               <h4 className="text-lg mb-2">{selectedEvent?.facility}</h4>
               <p className="mb-2">
                 {' '}
