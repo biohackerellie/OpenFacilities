77d651a865994e3ddcefe2a778765aab9f38781c
biohackerellie <epkerns@gmail.com>
2023-11-29 12:19:59 -0700

    Remove adminBool context file

--- front-end/src/components/contexts/adminBool.ts
+++
@@ -1,9 +0,0 @@
-import { getCurrentUser } from '@/functions/data/auth';
-
-export default async function isUserAdmin() {
-  const user = await getCurrentUser();
-  if (user) {
-    return user.admin;
-  }
-  return false;
-}
