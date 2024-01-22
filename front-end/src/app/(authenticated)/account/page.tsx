ebb300d3de12ffde072ab3add2a09c327841867a
biohackerellie <epkerns@gmail.com>
2024-01-22 07:12:14 -0700

    another fix

--- front-end/src/app/(authenticated)/account/page.tsx
+++ front-end/src/app/(authenticated)/account/page.tsx
@@ -5,20 +5,27 @@ import { Reservation } from '@/lib/types';
 import { userReservations } from '@/functions/calculations/tableData';
 import { Separator } from '@/components/ui/separator';
 import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/lib/auth';
+import { GetUserById } from '@/lib/db/queries/users';
 
 import { getProfile } from '@/functions/data/users';
 
 async function getData() {
   try {
-    const userSession = await getProfile();
+    const userSession = await getServerSession(authOptions);
+    console.log('userSession', userSession);
 
-    const reservations: Reservation[] = userSession?.Reservation;
-
-    if (!reservations) {
+    if (!userSession) {
       return [];
+    } else if (userSession) {
+      const user = await GetUserById.execute({ id: userSession.user.id });
+      //@ts-expect-error
+      const reservations: Reservation[] = user?.Reservation;
+      if (!reservations) {
+        return [];
+      }
+      return userReservations(reservations);
     }
-
-    return userReservations(reservations);
   } catch (error) {
     return [];
   }
@@ -26,12 +33,16 @@ async function getData() {
 
 export default async function Account() {
   const data = await getData();
+  if (!data) {
+    return <div>loading...</div>;
+  }
   return (
     <div className="space-y-7">
       <div>
         <h3 className="text-lg font-medium">My Reservations</h3>
       </div>
       <Separator />
+
       <DataTable columns={columns} data={data} />
     </div>
   );
